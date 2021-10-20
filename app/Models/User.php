7873fbbc94156848f87d4485992b26df60d675a2
Pahrul Irfan <ip.5090@gmail.com>
2021-10-20 10:11:59 +0800

    uuids traits

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
+    use Uuids;
     use HasFactory, Notifiable;
 
     /**
