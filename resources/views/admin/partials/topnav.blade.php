36d416fc9794afead2a93e8ab9da5879f425c6a0
Pahrul Irfan <ip.5090@gmail.com>
2021-10-20 11:01:19 +0800

    perbaikan notif

--- resources/views/admin/partials/topnav.blade.php
+++ resources/views/admin/partials/topnav.blade.php
@@ -13,7 +13,7 @@
     </div>
 </form>
 <ul class="navbar-nav navbar-right">
-    <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
+    {{-- <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
                                                  class="nav-link notification-toggle nav-link-lg{{ Auth::user()->unreadNotifications->count() ? ' beep' : '' }}"><i
                 class="far fa-bell"></i></a>
         <div class="dropdown-menu dropdown-list dropdown-menu-right">
@@ -40,7 +40,7 @@
                 @endif
             </div>
         </div>
-    </li>
+    </li> --}}
     <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="{{ asset('assets/img/avatar/avatar-1.png') }}" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div>
